3be9789b89ba5779cca351aefc386be4fac026f7
Michael DeLibero <mldelibero@gmail.com>
2012-07-04 13:24:15 -0400

    moved random footprints into misc catagory

--- gedaLib/createTemps.py
+++ gedaLib/createTemps.py
@@ -7,17 +7,25 @@ import os
 
 fpath = os.path.dirname(os.path.realpath(__file__))
 sympath = os.path.join(fpath,"symbols")
+pcbpath = os.path.join(fpath,"footprints")
 
 #--------------------------------------------------------------------
 def create_gafrc():
-""" Create a gafrc file in the calling directory """
+    """ Create a gafrc file in the calling directory """
     symDirs = os.listdir(sympath)
     with open ("./gafrc","w") as gafrc:
+        gafrc.write(sympath + "\n")
         for dir in symDirs:
             gafrc.write(os.path.join(sympath,dir) + "\n")
 #--------------------------------------------------------------------
-
-
+def create_prj_temp():
+    """ Create a prj.prj file in the calling directory """
+    pcbDirs = os.listdir(pcbpath)
+    with open ("./prj.prj","w") as prj:
+        prj.write("elements-dir " + pcbpath + "\n")
+        for dir in pcbDirs:
+            prj.write("elements-dir " + os.path.join(pcbpath,dir) + "\n")
 
 if __name__== "__main__":
-    create_gafrc()
+    create_prj_temp()
+    
